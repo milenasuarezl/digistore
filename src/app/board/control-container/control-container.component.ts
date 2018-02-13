1f6932e2088f541b8c3585a39b96b2237a173429
Edwin Torres <codesandtags@gmail.com>
2018-02-13 13:52:55 -0500

    feat: Added logic to simulate the buttons and sounds

--- src/app/board/control-container/control-container.component.ts
+++ src/app/board/control-container/control-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-control-container',
@@ -7,9 +7,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControlContainerComponent implements OnInit {
 
-  constructor() { }
+  // Sounds
+  @ViewChild('startGameSound') startGameSound;
+  @ViewChild('gameOverSound') gameOverSound;
+  @ViewChild('laserSound') laserSound;
+  @ViewChild('goodSound') goodSound;
 
-  ngOnInit() {
+  // Controls
+  @ViewChild('upArrow') upArrow;
+  @ViewChild('leftArrow') leftArrow;
+  @ViewChild('rightArrow') rightArrow;
+  @ViewChild('downArrow') downArrow;
+
+  // Constants
+  private readonly TIME_DELAY_PRESS = 200;
+  private readonly TIME_DELAY_COLOR = 400;
+  private readonly TIME_UPDATE_COLOR = 1000;
+  private readonly TIME_TO_START_GAME = 3000;
+
+  constructor() {
   }
 
+  ngOnInit() {
+
+  }
+
+  public onStartGame(): void {
+    this.startGame();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+
+    switch (event.keyCode) {
+      case KEY_CODE.RIGHT_ARROW:
+        this.sayGood();
+        this.pressButtonEffect(this.rightArrow, 'pressed', this.TIME_DELAY_PRESS);
+        break;
+
+      case KEY_CODE.LEFT_ARROW:
+        this.sayGood();
+        this.pressButtonEffect(this.leftArrow, 'pressed', this.TIME_DELAY_PRESS);
+        break;
+
+      case KEY_CODE.UP_ARROW:
+        this.shotLaser();
+        this.pressButtonEffect(this.upArrow, 'pressed', this.TIME_DELAY_PRESS);
+        break;
+
+      case KEY_CODE.DOWN_ARROW:
+        this.sayGameOver();
+        this.pressButtonEffect(this.downArrow, 'pressed', this.TIME_DELAY_PRESS);
+        break;
+    }
+  }
+
+  private startGame() {
+    this.startGameSound.nativeElement.play();
+    setTimeout( () => this.generateRandomControl(), this.TIME_TO_START_GAME);
+
+  }
+
+  private generateRandomControl() {
+    const controls = [this.upArrow, this.leftArrow, this.rightArrow, this.downArrow];
+
+    setInterval(() => {
+      const indexRandom = Math.round(Math.random() * (controls.length - 1));
+      const control = controls[indexRandom];
+      this.pressButtonEffect(control, 'active', this.TIME_DELAY_COLOR);
+    }, this.TIME_UPDATE_COLOR);
+  }
+
+
+  private pressButtonEffect(button: ElementRef, colorClassName: string, timeDelay: number) {
+    const dom = button.nativeElement.querySelector('button');
+    dom.click();
+    dom.classList.add(colorClassName);
+
+    setTimeout(() => {
+      dom.classList.remove(colorClassName);
+    }, timeDelay);
+  }
+
+  private sayGameOver() {
+    this.gameOverSound.nativeElement.play();
+  }
+
+  private shotLaser() {
+    this.laserSound.nativeElement.play();
+  }
+
+  private sayGood() {
+    this.goodSound.nativeElement.play();
+  }
+}
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40
 }
--- src/app/board/main-board/main-board.component.ts
+++ src/app/board/main-board/main-board.component.ts
@@ -1,67 +1,11 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-main-board',
-  templateUrl: './main-board.component.html',
-  styleUrls: ['./main-board.component.css']
+  selector: "app-main-board",
+  templateUrl: "./main-board.component.html",
+  styleUrls: ["./main-board.component.css"]
 })
 export class MainBoardComponent implements OnInit {
-
-  @ViewChild('startGameSound') startGameSound;
-  @ViewChild('gameOverSound') gameOverSound;
-  @ViewChild('laserSound') laserSound;
-  @ViewChild('goodSound') goodSound;
-
-  constructor() {
+  ngOnInit(): void {
   }
-
-  ngOnInit() {
-    this.startGame();
-  }
-
-  @HostListener('window:keyup', ['$event'])
-  keyEvent(event: KeyboardEvent) {
-    console.log(event);
-
-    switch (event.keyCode) {
-      case KEY_CODE.RIGHT_ARROW:
-        this.sayGood();
-        break;
-
-      case KEY_CODE.LEFT_ARROW:
-        this.sayGood();
-        break;
-
-      case KEY_CODE.UP_ARROW:
-        this.shotLaser();
-        break;
-
-      case KEY_CODE.DOWN_ARROW:
-        this.sayGameOver();
-        break;
-    }
-  }
-
-  private startGame() {
-    this.startGameSound.nativeElement.play();
-  }
-
-  private sayGameOver() {
-    this.gameOverSound.nativeElement.play();
-  }
-
-  private shotLaser() {
-    this.laserSound.nativeElement.play();
-  }
-
-  private sayGood() {
-    this.goodSound.nativeElement.play();
-  }
-}
-
-export enum KEY_CODE {
-  RIGHT_ARROW = 39,
-  LEFT_ARROW = 37,
-  UP_ARROW = 38,
-  DOWN_ARROW = 40
 }
