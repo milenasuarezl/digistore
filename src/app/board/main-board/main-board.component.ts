1fffade414f2a714bd463b2164d9bd5fd6a30297
Edwin Torres <codesandtags@gmail.com>
2018-02-12 21:55:03 -0500

    feat: Added sounds

--- src/app/board/main-board/main-board.component.ts
+++ src/app/board/main-board/main-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-board',
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainBoardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('startGameSound') startGameSound;
+  @ViewChild('gameOverSound') gameOverSound;
+  @ViewChild('laserSound') laserSound;
+  @ViewChild('goodSound') goodSound;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.startGame();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+
+    switch (event.keyCode) {
+      case KEY_CODE.RIGHT_ARROW:
+        this.sayGood();
+        break;
+
+      case KEY_CODE.LEFT_ARROW:
+        this.sayGood();
+        break;
+
+      case KEY_CODE.UP_ARROW:
+        this.shotLaser();
+        break;
+
+      case KEY_CODE.DOWN_ARROW:
+        this.sayGameOver();
+        break;
+    }
+  }
+
+  private startGame() {
+    this.startGameSound.nativeElement.play();
+  }
+
+  private sayGameOver() {
+    this.gameOverSound.nativeElement.play();
+  }
+
+  private shotLaser() {
+    this.laserSound.nativeElement.play();
+  }
+
+  private sayGood() {
+    this.goodSound.nativeElement.play();
+  }
+}
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40
 }
